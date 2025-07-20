0efd8d646486309e18b281edf3c87ebb3e4e0a08
assanalibatyrkhan <asankek12@gmail.com>
2025-07-20 11:01:59 +0800

    20.07.2025

---
+++ medium_33.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[l]:
+                if nums[l] <= target <= nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] <= target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+
+solution = Solution()
+print(solution.search(nums = [8,1,2,3,4,5,6,7], target = 6))
