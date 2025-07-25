f312465588dd5e80d9a8decd727ce5632a5fd3f7
assanalibatyrkhan <asankek12@gmail.com>
2025-07-25 15:19:44 +0800

    25.07.2025

--- LinkedListGenerator/LinkedListGenerator.py
+++ LinkedListGenerator/LinkedListGenerator.py
@@ -4,10 +4,7 @@ class ListNode:
         self.next = next
 
 def generateLinkedListFromArray(arr):
-    reversedLinkedList = [ListNode(arr[len(arr) - 1], None)]
-    for i in range(len(arr) - 2, -1, -1):
-        reversedLinkedList.append(ListNode(arr[i]))
-    linkedList = reversedLinkedList[::-1]
+    linkedList = [ListNode(arr[i]) for i in range(len(arr))]
     for i in range(len(linkedList) - 1):
         linkedList[i].next = linkedList[i + 1]
     return linkedList[0]
