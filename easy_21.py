4ba492ca42b500116232068a9ba6c1e465b844b2
assanalibatyrkhan <asankek12@gmail.com>
2025-07-26 15:32:52 +0800

    26.07.2025

--- easy_21.py
+++ easy_21.py
@@ -1,3 +1,4 @@
+from LinkedListGenerator.LinkedListGenerator import generateLinkedListFromArray
 from typing import Optional
 
 
@@ -11,44 +12,28 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         curr1 = list1
         curr2 = list2
-        head = curr = ListNode()
-        while curr1 and curr2:
-            if curr1.val < curr2.val:
-                # print("first", curr1.val)
-                curr.next = curr1
-                curr1 = curr1.next
-            else:
-                # print("second", curr2.val)
-                curr.next = curr2
-                curr2 = curr2.next
-            # print(curr.val)
-            curr = curr.next
-            # print(curr)
-        else:
+        head = curr3 = ListNode()
+        while curr1 or curr2:
             if curr1:
-                while curr1:
-                    # print(curr1.val)
-                    curr.next = curr1
+                if curr2: # general
+                    if curr1.val < curr2.val:
+                        # print("first", curr1.val)
+                        curr3.next = curr1
+                        curr1 = curr1.next
+                    else:
+                        # print("second", curr2.val)
+                        curr3.next = curr2
+                        curr2 = curr2.next
+                else: # if curr2 is none
+                    curr3.next = curr1
+                    # print(curr3.val)
                     curr1 = curr1.next
-                    curr = curr.next
-            else:
-                while curr2:
-                    # print(curr2.val)
-                    curr.next = curr2
-                    curr2 = curr2.next
-                    curr = curr.next
+            else: # if curr 1 is none
+                curr3.next = curr2
+                # print(curr3.val)
+                curr2 = curr2.next
+            curr3 = curr3.next
         return head.next
 
-
-if __name__ == '__main__':
-    a11 = ListNode(1)
-    a12 = ListNode(2)
-    a13 = ListNode(4)
-    a11.next = a12
-    a12.next = a13
-    a21 = ListNode(1)
-    a22 = ListNode(3)
-    a23 = ListNode(4)
-    a21.next = a22
-    a22.next = a23
-    print(Solution().mergeTwoLists(a11, a21).next.next.next.val)
+solution = Solution()
+# print(solution.mergeTwoLists(generateLinkedListFromArray([]), generateLinkedListFromArray([])))
