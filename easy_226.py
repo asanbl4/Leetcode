429d1e52343570b309348f5b02a77ec09466aa37
assanalibatyrkhan <asankek12@gmail.com>
2025-08-04 09:28:09 +0500

    04.08.2025

--- easy_226.py
+++ easy_226.py
@@ -1,6 +1,18 @@
+from typing import Optional
+from BinaryTreeGenerator.BinaryTreeGenerator import generateBinaryTree
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
+
+solution = Solution()
+print(solution.invertTree(generateBinaryTree([2,1,3])).right.val)
